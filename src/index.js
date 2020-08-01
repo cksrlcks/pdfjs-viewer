4b1f6efbfbb9241e0e6a7ae65b0f81408d90cef5
chanki <chanki.kim89@gmail.com>
2020-08-01 22:34:33 +0900

    second commit

--- src/index.js
+++ src/index.js
@@ -1,156 +1,362 @@
-(function () {
-  var pdfDoc = null;
-  var pageNum = 1;
-  var totalPageNum = null;
-  var pageRendering = false;
-  var pageNumPending = null;
-  var scale = 1;
-  var pdfContainer = document.querySelector("#pdf-view");
-  var canvas = null;
-
-  window.initUxisViewer = function (url) {
-    pdfjsLib.getDocument(url).promise.then(function (pdf) {
-      pdfDoc = pdf;
-      totalPageNum = pdf.numPages;
-      document.querySelector(".total").innerHTML = totalPageNum;
-      //initZoom();
-      //inintDownload(url);
-      //initCounter();
-      //initPager();
-      //renderPage(pageNum);
-      drawCanvas();
-      renderPage(pageNum);
-    });
-  };
-
-  // function initZoom() {
-  //   const zoomInButton = document.querySelector("#zoominbutton");
-  //   const zoomOutButton = document.querySelector("#zoomoutbutton");
-  //   const zoomResetButton = document.querySelector("#zoomresetbutton");
-
-  //   zoomInButton.addEventListener("click", function () {
-  //     if (pdfScale >= 2) {
-  //       return;
-  //     }
-
-  //     pdfScale += 0.25;
-  //     render(pdfScale);
-  //   });
-
-  //   zoomOutButton.addEventListener("click", function () {
-  //     if (pdfScale < 0.75) {
-  //       return;
-  //     }
-
-  //     pdfScale -= 0.25;
-  //     render(pdfScale);
-  //   });
-
-  //   zoomResetButton.addEventListener("click", function () {
-  //     pdfScale = 1;
-  //     render(pdfScale);
-  //   });
-  // }
-
-  // function inintDownload(url) {
-  //   const download_button = document.querySelector(".buttons .download");
-  //   download_button.setAttribute("href", url);
-  // }
-
-  // function setCurrentCount() {
-  //   const current_box = document.querySelector(".count .current");
-  //   current_box.innerHTML = currentPageIndex + 1;
-  // }
-
-  // function initCounter() {
-  //   const total_box = document.querySelector(".count .total");
-
-  //   total_box.innerHTML = totalPagesCount;
-  //   setCurrentCount();
-  // }
-
-  // function pagerAction(e) {
-  //   const action = e.target.getAttribute("data-pager");
-  //   if (action === "prev") {
-  //     if (currentPageIndex === 0) {
-  //       return;
-  //     }
-  //     currentPageIndex -= pageMode;
-  //     if (currentPageIndex < 0) {
-  //       currentPageIndex = 0;
-  //     }
-  //     render();
-  //   }
-  //   if (action === "next") {
-  //     if (currentPageIndex === totalPagesCount - 1) {
-  //       return;
-  //     }
-  //     currentPageIndex += pageMode;
-  //     if (currentPageIndex > totalPagesCount - 1) {
-  //       currentPageIndex = totalPagesCount - 1;
-  //     }
-  //     render();
-  //   }
-  // }
-
-  // function initPager() {
-  //   const pager = document.querySelector("#pdf-pager");
-  //   pager.addEventListener("click", pagerAction);
-  // }
-
-  function drawCanvas() {
-    const pagesHTML = `<div style="width=100%" class="canvas-container"><canvas id="pdf-canvas"></canvas></div>`;
-    pdfContainer.innerHTML = pagesHTML;
-    canvas = document.querySelector("#pdf-canvas");
-  }
-
-  function renderPage(pageNum) {
-    pageRendering = true;
-
-    pdfDoc.getPage(pageNum).then(function (page) {
-      scale = pdfContainer.children[0].offsetWidth / page.getViewport({ scale: 1 }).width;
-
-      var viewport = page.getViewport({ scale: scale });
-      canvas.height = viewport.height;
-      canvas.width = viewport.width;
-
-      var renderContext = {
-        canvasContext: canvas.getContext("2d"),
-        viewport: viewport,
-      };
-
-      var renderTask = page.render(renderContext);
-
-      renderTask.promise.then(function () {
-        pageRendering = false;
-        if (pageNumPending !== null) {
-          renderPage(pageNumPending);
-          pageNumPending = null;
+"use strict";
+//ie forEach polyfill
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (var i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
         }
-      });
+    };
+}
 
-      document.querySelector(".current").innerHTML = pageNum;
-    });
-  }
-
-  function debounce(func, wait, immediate) {
+function debounce(func, wait, immediate) {
     var timeout;
     return function () {
-      var context = this,
-        args = arguments;
-      var later = function () {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+        var context = this,
+            args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
     };
-  } // 디바운스 설정
+}
 
-  window.addEventListener(
-    "resize",
-    debounce(() => renderPage(pageNum), 150)
-  );
-})();
+var UxisViewer = function (options) {
+    this.url = options.url;
+    this.mode = options.mode;
+    this.target = document.querySelector(options.target);
+
+    this.pdfDoc = null;
+    this.pageNum = 1;
+    this.currentPageNum = 1;
+    this.totalPageNum = null;
+    this.pageRendering = false;
+    this.pageNumPending = null;
+    this.scale = 1;
+    this.canvas = null;
+    this.viewport = null;
+    this.PDFJS = PDFJS;
+    this.zoom_in_scale = null;
+
+    this.update_pager = function () {
+        var current = this.currentPageNum;
+        this.target.querySelector(".current").innerHTML = current;
+    };
+
+    this.set_pager = function () {
+        var total = this.totalPageNum;
+        this.target.querySelector(".total").innerHTML = total;
+
+        this.update_pager();
+    };
+
+    this.handle_buttons = function (e) {
+        var _this = this;
+
+        var action = e.target.getAttribute("data-button");
+        if (action === "prev") {
+            if (_this.currentPageNum === 1) {
+                return;
+            }
+            _this.currentPageNum--;
+
+            _this.pdfDoc.getPage(_this.currentPageNum).then(function (page) {
+                _this.handlePages(page);
+            });
+
+            _this.update_pager();
+        }
+
+        if (action === "next") {
+            if (_this.currentPageNum === _this.totalPageNum) {
+                return;
+            }
+            _this.currentPageNum++;
+
+            _this.pdfDoc.getPage(_this.currentPageNum).then(function (page) {
+                _this.handlePages(page);
+            });
+
+            _this.update_pager();
+        }
+
+        if (action === "download") {
+            var link = document.createElement("a");
+            link.href = _this.url;
+            link.download = _this.url;
+            link.click();
+        }
+
+        if (action === "external") {
+            var external = window.open(_this.url);
+        }
+        if (action === "print") {
+            var print_window = window.open(_this.url);
+            print_window.print();
+        }
+        if (action === "zoom_in") {
+            if (_this.scale >= 2) {
+                return;
+            }
+            _this.zoom_in_scale += 0.25;
+            _this.resize_render(_this.zoom_in_scale);
+        }
+        if (action === "zoom_out") {
+            if (_this.scale < 1.2) {
+                return;
+            }
+            _this.zoom_in_scale -= 0.25;
+            _this.resize_render(_this.zoom_in_scale);
+        }
+        if (action === "zoom_reset") {
+            _this.zoom_in_scale = 0;
+            _this.resize_render(_this.zoom_in_scale);
+        }
+    };
+
+    this.buttons_actions = this.handle_buttons.bind(this);
+
+    this.viewer_buttons = function () {
+        var _this = this;
+        var buttons = document.querySelector(".uxis-pdf-buttons");
+        buttons.addEventListener("click", _this.buttons_actions);
+    };
+
+    this.renderPage = function (scale) {
+        var _this = this;
+        var scale = scale;
+        //풀페이지 랜더링 과정
+        _this.pdfDoc.getPage(1).then(function (page, scale) {
+            _this.handlePages(page, scale);
+        });
+    };
+
+    this.resize_handler = function () {
+        var _this = this;
+        window.addEventListener(
+            "resize",
+            debounce(function () {
+                _this.resize_render();
+            }, 150)
+        );
+    };
+
+    this.resize_render = function (scale) {
+        var _this = this;
+
+        //기존 캔버스 삭제
+        var all_canvas = _this.target.querySelectorAll(".pdf-canvas");
+        all_canvas.forEach(function (item) {
+            item.remove();
+        });
+
+        if (this.mode === "one") {
+            //원페이지일경우 리랜더링 과정
+            _this.pdfDoc.getPage(_this.currentPageNum).then(function (page) {
+                _this.handlePages(page, scale);
+            });
+        } else {
+            //풀페이지일경우 리랜더링 과정
+            _this.pdfDoc.getPage(1).then(function (page) {
+                _this.currentPageNum = 1;
+                _this.handlePages(page, scale);
+            });
+        }
+    };
+
+    this.initUxisViewer = function () {
+        var _this = this;
+
+        // 프레임 그리기
+        _this.createFrame();
+
+        // pdf정보 가져오기
+        _this.PDFJS.getDocument(this.url).then(function (pdf) {
+            _this.pdfDoc = pdf;
+            _this.totalPageNum = pdf.numPages;
+
+            //원페이지 모드때 페이저 정보 입력
+            _this.mode === "one" && _this.set_pager();
+
+            //뷰어 기능 활성화 시작
+            _this.viewer_buttons();
+
+            //페이지 랜더링 시작
+            _this.renderPage();
+        });
+    };
+
+    this.handlePages = function (page, scale) {
+        //메소드 내부안에서 this의 바인딩을 위해
+        var _this = this;
+
+        var canvas = document.createElement("canvas");
+        canvas.style.display = "block";
+        canvas.classList.add("pdf-canvas");
+        _this.scale = _this.target.querySelector(".uxis-pdf-view .canvas-container").offsetWidth / page.getViewport(1).width;
+
+        if (scale) {
+            _this.scale += scale;
+        }
+
+        _this.viewport = page.getViewport(_this.scale);
+
+        var context = canvas.getContext("2d");
+        canvas.height = _this.viewport.height;
+        canvas.width = _this.viewport.width;
+
+        page.render({
+            canvasContext: context,
+            viewport: _this.viewport,
+        });
+
+        if (_this.mode === "one") {
+            //이전 페이지 삭제
+            var prev_canvas = _this.target.querySelector(".pdf-canvas");
+            prev_canvas && prev_canvas.remove();
+
+            _this.target.querySelector(".canvas-container").appendChild(canvas);
+            if (_this.pageNumPending !== null) {
+                _this.handlePages(_this.pageNumPending);
+                _this.pageNumPending = null;
+            }
+        } else {
+            _this.target.querySelector(".canvas-container").appendChild(canvas);
+            _this.currentPageNum++;
+
+            if (_this.pdfDoc !== null && _this.currentPageNum <= _this.totalPageNum) {
+                _this.pdfDoc.getPage(_this.currentPageNum).then(function (page) {
+                    _this.handlePages(page, scale);
+                });
+            }
+        }
+    };
+
+    this.init = function () {
+        this.initUxisViewer();
+        this.resize_handler();
+    };
+
+    return this.init();
+};
+
+UxisViewer.prototype.createButton = function (info) {
+    var uxisPdfButtons = document.createElement("div");
+    uxisPdfButtons.classList.add("uxis-pdf-buttons");
+
+    var button_length = info.length;
+    for (var i = 0; i < button_length; i++) {
+        var action = info[i].action;
+        var title = info[i].title;
+
+        var button = document.createElement("button");
+        button.classList.add("button_" + action);
+        button.setAttribute("data-button", action);
+        button.setAttribute("type", "button");
+        button.setAttribute("title", title);
+
+        var hidden_title = document.createElement("i");
+        hidden_title.classList.add("hidden");
+        hidden_title.innerHTML = title;
+        button.appendChild(hidden_title);
+
+        uxisPdfButtons.appendChild(button);
+    }
+
+    return uxisPdfButtons;
+};
+
+UxisViewer.prototype.createFrame = function () {
+    var uxisViewer = document.createElement("div");
+    uxisViewer.classList.add("uxis-viewer");
+
+    var uxisToolbar = document.createElement("div");
+    uxisToolbar.classList.add("uxis-toolbar");
+
+    var menuObject = null;
+
+    if (this.mode === "one") {
+        var count = document.createElement("div");
+        count.classList.add("count");
+        count.innerHTML = "<span class='current'></span> / <span class='total'></span>";
+
+        uxisToolbar.appendChild(count);
+
+        menuObject = [
+            {
+                action: "prev",
+                title: "이전페이지",
+            },
+            {
+                action: "next",
+                title: "다음페이지",
+            },
+            {
+                action: "download",
+                title: "다운로드",
+            },
+            {
+                action: "external",
+                title: "새창에서 보기",
+            },
+            {
+                action: "print",
+                title: "프린트",
+            },
+            {
+                action: "zoom_in",
+                title: "확대보기",
+            },
+            {
+                action: "zoom_out",
+                title: "축소하기",
+            },
+            {
+                action: "zoom_reset",
+                title: "RESET",
+            },
+        ];
+    } else {
+        menuObject = [
+            {
+                action: "download",
+                title: "다운로드",
+            },
+            {
+                action: "external",
+                title: "새창에서 보기",
+            },
+            {
+                action: "print",
+                title: "프린트",
+            },
+            {
+                action: "zoom_in",
+                title: "확대보기",
+            },
+            {
+                action: "zoom_out",
+                title: "축소하기",
+            },
+            {
+                action: "zoom_reset",
+                title: "RESET",
+            },
+        ];
+    }
+
+    var buttons = this.createButton(menuObject);
+
+    var pdfContainer = document.createElement("div");
+    pdfContainer.classList.add("uxis-pdf-container");
+    pdfContainer.innerHTML = '<div class="uxis-pdf-view"><div style="width:100%" class="canvas-container"></div></div>';
+
+    uxisToolbar.appendChild(buttons);
+    uxisViewer.appendChild(uxisToolbar);
+    uxisViewer.appendChild(pdfContainer);
+    this.target.appendChild(uxisViewer);
+};
